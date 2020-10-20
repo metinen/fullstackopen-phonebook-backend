34be954040eb4ad1d46f31a8d7b848fb6fe7d3de
metinen <maria.ilvonen@gmail.com>
2020-10-20 16:06:10 +0300

    uibuild

---
+++ build/precache-manifest.5ec41ac8e2234c9aa85413492e5f1f73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80989dd6d09771105d888644daf10251",
+    "url": "/index.html"
+  },
+  {
+    "revision": "efbd71ea7281f8a36ba0",
+    "url": "/static/css/main.a46f1593.chunk.css"
+  },
+  {
+    "revision": "740e955edf9b73a83760",
+    "url": "/static/js/2.584d881a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.584d881a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efbd71ea7281f8a36ba0",
+    "url": "/static/js/main.79efcfe1.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
